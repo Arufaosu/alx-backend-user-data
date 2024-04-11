e27d61519a57bfc0fd07626c43e203ba68d0dfb1
root <RobinLeeHenry101@gmail.com>
2024-04-11 15:04:04 +0000

    implemented filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,26 @@ import re
 import logging
 from typing import List
 
+
+class RedactingFormatter(logging.Formatter):
+    """redacting formatter"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields=[]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values"""
+        msg = filter_datum(self.fields, self.REDACTION,
+                           record.msg, self.SEPARATOR)
+        msg = re.sub(r';(.)', r'; \1', msg)
+        record.msg = msg
+        return super().format(record)
+
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """returns the messages"""
     obf_msg = message
