8e84b98e85252299ba773c6578e538552de25948
root <RobinLeeHenry101@gmail.com>
2024-04-11 14:15:21 +0000

    reset all the code, and should be correct for task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,25 +1,14 @@
 #!/usr/bin/env python3
+"""filter logger"""
 import re
 import logging
+from typing import List
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields=()):
-        super().__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        message = record.getMessage()
-        for field in self.fields:
-            message = filter_datum([field], self.REDACTION, message, self.SEPARATOR)
-        record.msg = message
-        return super().format(record)
-
-def filter_datum(fields, redaction, message, separator):
-    return re.sub(r'(?<=\b|^)(' + '|'.join(fields) + r')=[^{}]+(?=\b|$)'.format(re.escape(separator)), lambda x: x.group().split('=')[0] + '=' + redaction, message)
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """returns the messages"""
+    obf_msg = message
+    for f in fields:
+        obf_msg = re.sub(r'{}=.*?{}'.format(f, separator),
+                         f'{f}={redaction}{separator}', obf_msg)
+    return obf_msg
 
